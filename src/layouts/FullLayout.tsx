1928a70580b12c976f33ccf14e6efacfda47492b
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-12-03 15:10:19 +0900

    레이아웃 형태 변경

--- src/layouts/FullLayout.tsx
+++ src/layouts/FullLayout.tsx
@@ -1,17 +1,53 @@
 import React, {ReactElement} from 'react';
-import {Layout} from 'antd';
+import {Layout, Menu} from 'antd';
 
-import {HeaderComponent} from '@/layouts';
+import {UserOutlined, VideoCameraOutlined, UploadOutlined} from '@ant-design/icons';
+import {NavLink} from 'react-router-dom';
+import {routes} from '@/router';
+
+const {Header, Content, Sider, Footer} = Layout;
 
 // Header와 Footer를 갖고 있는 layout입니다. HOC
 function WithFullLayout(WrappedComponent: any): any {
   return class extends React.Component {
     render(): ReactElement {
       return (
-        <Layout className="app">
-          <HeaderComponent />
-          {/* WrappedComponent는 반드시 Layout으로 wrapping되어야 하며, Layout.Content를 포함해야한다 */}
-          <WrappedComponent {...this.props} />
+        <Layout className="app" style={{minHeight: '100vh'}}>
+          <Sider
+            breakpoint="lg"
+            collapsedWidth="0"
+            onBreakpoint={(broken) => {
+              console.log(broken);
+            }}
+            onCollapse={(collapsed, type) => {
+              console.log(collapsed, type);
+            }}
+          >
+            <div className="logo" />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+              {routes
+                .filter((route) => route.meta.navigation.show)
+                .map((route) => {
+                  return (
+                    <Menu.Item key={route.path}>
+                      <NavLink to={route.path} exact={route.exact} activeClassName="active">
+                        {route.name}
+                      </NavLink>
+                    </Menu.Item>
+                  );
+                })}
+            </Menu>
+          </Sider>
+          <Layout>
+            <Header className="site-layout-sub-header-background" style={{color: 'white', textAlign: 'center', padding: 0}}>
+              header 영역
+            </Header>
+            <Content style={{margin: '24px 16px 0'}}>
+              {/* WrappedComponent는 반드시 Layout으로 wrapping되어야 하며, Layout.Content를 포함해야한다 */}
+              <WrappedComponent {...this.props} />
+            </Content>
+            <Footer style={{textAlign: 'center'}}>footer 영역</Footer>
+          </Layout>
         </Layout>
       );
     }
---
+++ src/layouts/FullLayout3.tsx
@@ -0,0 +1,22 @@
+import React, {ReactElement} from 'react';
+import {Layout} from 'antd';
+
+import {HeaderComponent} from '@/layouts';
+
+// Header와 Footer를 갖고 있는 layout입니다. HOC
+function WithFullLayout(WrappedComponent: any): any {
+  return class extends React.Component {
+    render(): ReactElement {
+      return (
+        <Layout className="app" style={{minHeight: '100vh'}}>
+          <HeaderComponent />
+
+          {/* WrappedComponent는 반드시 Layout으로 wrapping되어야 하며, Layout.Content를 포함해야한다 */}
+          <WrappedComponent {...this.props} />
+        </Layout>
+      );
+    }
+  };
+}
+
+export default WithFullLayout;
