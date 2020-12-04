43d8403145e81bcfb6d44eac4c4afe942b371c49
jaehong.lee <jaehong.lee@solarconnect.kr>
2020-12-04 17:04:30 +0900

    라우터 간소화

--- src/router/routes.js
+++ src/router/routes.js
@@ -12,120 +12,21 @@ const routes = [
     name: 'root',
     exact: true,
     component: lazy(() => import('@/pages/home/Container')),
-    meta: {
-      navigation: {
-        show: false,
-      },
-    },
   },
   {
     path: '/home',
     name: 'home',
-    component: lazy(() => {
-      return new Promise((resolve) => {
-        setTimeout(() => resolve(import('@/pages/home/Container')), 1000);
-      });
-    }),
-    meta: {
-      navigation: {
-        show: true,
-      },
-    },
+    component: lazy(() => import('@/pages/home/Container')),
   },
   {
     path: '/recoil',
     name: 'recoil',
-    component: lazy(() => {
-      return new Promise((resolve) => {
-        setTimeout(() => resolve(import('@/pages/recoil/Container')), 1000);
-      });
-    }),
-    meta: {
-      navigation: {
-        show: true,
-      },
-    },
+    component: lazy(() => import('@/pages/recoil/Container')),
   },
   {
     path: '*',
     component: NotFound,
-    meta: {
-      navigation: {
-        show: false,
-      },
-    },
   },
-  // setTimeout 왜걸어놨지?
-  // {
-  //   path: '/home',
-  //   name: 'home',
-  //   component: lazy(() => {
-  //     return new Promise((resolve) => {
-  //       setTimeout(() => resolve(import('@/pages/test/Container')), 1000);
-  //     });
-  //   }),
-  //   meta: {
-  //     navigation: {
-  //       show: true,
-  //     },
-  //   },
-  // },
-  // {
-  //   path: '/home',
-  //   name: 'home',
-  //   component: lazy(() => import('@/pages/home/Container')),
-  //   meta: {
-  //     navigation: {
-  //       show: true,
-  //     },
-  //   },
-  // },
-  // {
-  //   path: '/test',
-  //   name: 'test',
-  //   component: lazy(() => import('@/pages/test/Container')),
-  //   meta: {
-  //     navigation: {
-  //       show: true,
-  //     },
-  //   },
-  // },
-  // {
-  //   path: '/example',
-  //   name: 'example (use Layout)',
-  //   component: lazy(() => {
-  //     return new Promise((resolve) => {
-  //       setTimeout(() => resolve(import('@/components/example/Container')), 1000);
-  //     });
-  //   }),
-  //   meta: {
-  //     navigation: {
-  //       show: true,
-  //     },
-  //   },
-  //   routes: [ // 하위 라우터
-  //     {
-  //       path: '/example/full',
-  //       name: 'FullLayout(Sider and Content)',
-  //       component: lazy(() => import('@/components/example/full/Container')),
-  //       meta: {
-  //         navigation: {
-  //           show: true,
-  //         },
-  //       },
-  //     },
-  //     {
-  //       path: '/example/content',
-  //       name: 'Content Layout(Only Content)',
-  //       component: lazy(() => import('@/components/example/content/Container')),
-  //       meta: {
-  //         navigation: {
-  //           show: true,
-  //         },
-  //       },
-  //     },
-  //   ],
-  // },
 ];
 
 export default routes;
